5532a791f4c3070761ac77007e38cbdd38666822
Dora Hagen <dorahagen@dora-hagens-mbp.att.net>
2014-06-24 18:46:30 -0500

    Added functionality for 4 buttons

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+	// Linked In
+
 	$('.linkedin-off').mouseenter(function() {
 		$(this).hide();
 		$('.linkedin-on').show()
@@ -20,4 +23,77 @@ $(document).ready(function() {
 		).hide();
 		$('.linkedin-off').show();
 	});
+
+	// Twitter
+
+	$('.twitter-off').mouseenter(function() {
+		$(this).hide();
+		$('.twitter-on').show()
+		.animate (
+			{'left': '0px'},
+			100,
+			function() {
+				// slide out complete
+			}
+		);
+	});
+	$('.twitter-on').mouseleave(function() {
+		$(this).animate (
+			{'left': '-174px'},
+			500,
+			function() {
+				// slide in complete
+			}
+		).hide();
+		$('.twitter-off').show();
+	});
+
+	// Behance
+
+	$('.behance-off').mouseenter(function() {
+		$(this).hide();
+		$('.behance-on').show()
+		.animate (
+			{'left': '0px'},
+			100,
+			function() {
+				// slide out complete
+			}
+		);
+	});
+	$('.behance-on').mouseleave(function() {
+		$(this).animate (
+			{'left': '-174px'},
+			500,
+			function() {
+				// slide in complete
+			}
+		).hide();
+		$('.behance-off').show();
+	});
+
+	// Email
+
+	$('.email-off').mouseenter(function() {
+		$(this).hide();
+		$('.email-on').show()
+		.animate (
+			{'left': '0px'},
+			100,
+			function() {
+				// slide out complete
+			}
+		);
+	});
+	$('.email-on').mouseleave(function() {
+		$(this).animate (
+			{'left': '-174px'},
+			500,
+			function() {
+				// slide in complete
+			}
+		).hide();
+		$('.email-off').show();
+	});
+
 });
